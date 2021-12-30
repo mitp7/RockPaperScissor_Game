7c149f266525f493cc79603bc995ecb1bd4b682c
Mit Patel <mitpatel2752@gmail.com>
2021-12-29 19:51:55 -0500

    Choosing functionality added

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,9 +1,81 @@
+let playerScore = 0;
+const paper = document.getElementById('paper');
+const rock = document.getElementById('rock');
+const scissors = document.getElementById('scissors');
+const options = document.getElementsByClassName('selector')[0];
+const gameScore = document.getElementById('score');
+
+const inGame = document.getElementsByClassName('in-game')[0];
+
+
+function computerChoice() {
+    const choices = ['paper', 'rock', 'scissors'];
+    const randomChoice = Math.floor(Math.random() * 3);
+    const compChoice = choices[randomChoice];
+    return compChoice;
+}
+
+function game(playerChoice){
+    //const compChoice = computerChoice();
+    const compChoice = 'scissors';
+    const overallChoice = playerChoice + compChoice;
+    options.style.display = 'none';
+
+    switch (overallChoice) {
+        // Player Wins
+        case 'rockscissors':
+        case 'scissorspaper':
+        case 'paperrock':
+            playerScore += 1;
+
+            inGame.innerHTML = `
+            <div class="final-selection">
+                <div class="player-pick">
+                    <h1 class="player-title">YOU PICKED</h1>
+                    <div class="background-icon background-${playerChoice}">
+                        <div class="selection">
+                            <img src = "images/icon-${playerChoice}.svg" id="${playerChoice}" alt="${playerChoice}">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="computer-pick">
+                    <h1 class="comp-title">THE HOUSE PICKED</h1>
+                    <div class="background-icon background-${compChoice}">
+                        <div class="selection">
+                            <img src = "images/icon-${compChoice}.svg" id="${compChoice}" alt="${compChoice}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+            //Update score 
+            gameScore.innerHTML = playerScore;
+            break;
+    }
+}
+
+function main () {
+    paper.addEventListener('click', () => {
+        game('paper');
+    })
+
+    rock.addEventListener('click', () => {
+        game('rock');
+    })
+
+    scissors.addEventListener('click', () => {
+        game('scissors');
+    })
+}
+
+
 // Get rules modal element 
-var modal = document.getElementById('simpleModal');
+const modal = document.getElementById('simpleModal');
 // Get button which opens the modal
-var modalBtn = document.getElementById('openBtn');
+const modalBtn = document.getElementById('openBtn');
 // Get button which closes the modal
-var closeBtn = document.getElementById('closeBtn');
+const closeBtn = document.getElementById('closeBtn');
 
 // Listen for open modal click
 modalBtn.addEventListener('click', openModal);
@@ -18,3 +90,5 @@ function openModal() {
 function closeModal(){
     modal.style.display = 'none';
 }
+
+main();
